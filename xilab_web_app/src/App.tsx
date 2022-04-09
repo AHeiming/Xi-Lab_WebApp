2bfbd79e419287c089102beaec55efceeb5f569d
Alex Heiming <alexheiming@t-online.de>
2022-04-09 15:07:44 +0200

    Implement OpenStreetMap using leaflet

--- xilab_web_app/src/App.tsx
+++ xilab_web_app/src/App.tsx
@@ -1,7 +1,43 @@
 import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect, useState } from "react";
+import CustomMarker from "./CustomMarker";
 
 function App() {
-  return <></>;
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const geolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setLoaded(true);
+      });
+    } else {
+      setLat(51.533143);
+      setLng(6.932462);
+      setLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    geolocation();
+  }, []);
+
+  return loaded ? (
+    <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={[lat, lng]}>
+        <Popup>
+          <CustomMarker />
+        </Popup>
+      </Marker>
+    </MapContainer>
+  ) : (
+    <div>Loading...</div>
+  );
 }
 
 export default App;
---
+++ xilab_web_app/src/CustomMarker.tsx
@@ -0,0 +1,10 @@
+const CustomMarker = () => {
+  return (
+    <div>
+      <h2>Blumenbeet</h2>
+      <h3>Füllstand: 100%</h3>
+    </div>
+  );
+};
+
+export default CustomMarker;
